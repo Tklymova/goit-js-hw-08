f12afedcaa905f7ca19ced990aed76accea9ba22
Tklymova <127869831+Tklymova@users.noreply.github.com>
2023-09-16 21:18:58 +0100

    vars5

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const pictureContainer = document.querySelector('.gallery');
+
+const pictureMarkup = createColorCardsMarkup(galleryItems);
+
+pictureContainer.insertAdjacentHTML('beforeend', pictureMarkup);
+
+
+function createColorCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                    <a class="gallery__item" href="${original}">
+                        <img class="gallery__image"
+                            style="display: block"
+                            src="${preview}" 
+                            alt="${description}" />
+                    </a>
+            `;
+        })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionPosition: "buttom",
+    captionDelay: "250"
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+    
+const getPosition = function (currentPosition) {
+localStorage.setItem("videoplayer-current-time", JSON.stringify(currentPosition));
+}
+
+const playerON = player.on('timeupdate', throttle(getPosition, 1000))
+
+const pausedTime = localStorage.getItem("videoplayer-current-time");
+const startTime = JSON.parse(pausedTime);
+try {
+player.setCurrentTime(startTime.seconds || 0).then(function(seconds) {
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+            default:
+            break;
+    }
+});
+} catch (error) {
+
+}
+
