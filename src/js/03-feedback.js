2e9b7513d643133e108c06f0c04de6bd9be8ca96
Tklymova <127869831+Tklymova@users.noreply.github.com>
2023-09-16 21:30:49 +0100

    vers 6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = 'feedback-form-state';
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form  input'),
+    textarea: document.querySelector('.feedback-form  textarea'),
+};
+
+let formData = {};
+
+
+refs.form.addEventListener('submit', handelFormSubmit);
+refs.form.addEventListener('input', throttle(handelInput, 500));
+
+function handelInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+populateTextarea();
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const savedMessageParse = JSON.parse(savedMessage);
+try {
+    if (savedMessageParse.email) {
+        refs.input.value = savedMessageParse.email;
+        formData.email = savedMessageParse.email;
+    }; } catch (error) {}
+try {
+    if (savedMessageParse.message) {
+        refs.textarea.value = savedMessageParse.message;
+        formData.message = savedMessageParse.message;
+    };
+} catch (error) {}
+}
+
+
+function handelFormSubmit(event) {
+
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        return
+    } else {
+        event.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        console.log(formData);
+        formData = {};
+    }
+}
